da68b00e00ddd75deafcd02abaeff6226fd4e2ec
Raja <aravindha92@gmail.com>
2023-05-08 08:59:35 +0530

    add animations

--- src/components/materialUI/Buttons.jsx
+++ src/components/materialUI/Buttons.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-
+import './Buttons.css'
 export default function ContainedButtons() {
   return (
-    <div className='mt-5'>
+    <div className='mt-5 buttons'>
       <button className='bg-sky-500 rounded-md py-1 px-3 '>View Courses</button>
       <button className='bg-gray-400 rounded-md ml-2 py-1 px-3'>Watch Video</button>
     </div>
