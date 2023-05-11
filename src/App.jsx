123db178b1e754eb6216fa1b6e53552262edfc04
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-05-11 15:39:09 +0530

    testm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import Banner from "./components/banner/Banner";
 import Navbar from "./components/nav/Navbar";
 import SocialMedia from "./components/Cards/SocialMedia"
+import Scp from './components/testimons/Scp';
 
 function App() {
   return (
@@ -15,14 +17,17 @@ function App() {
               <div className=""> 
                 <Banner ></Banner>
               </div>
-
           </div>
       </div>
       <div  className=" bg-gray-800">
         <div className="mx-60">
-                  <SocialMedia></SocialMedia>
+              <SocialMedia></SocialMedia>
         </div>
       </div>
+      <div className="bg-[#111827]">
+          <Scp></Scp>
+      </div>
+
       
     </div>
   );
---
+++ src/components/testimons/Scp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Scp() {
+  return (
+    <div>
+        <div className='text-center text-white'>
+            <div className='text-[1rem] pt-10 font-[Rubik]'>WHY CODING-DECODED ?</div>
+            <br />
+            <div className='text-[2rem] font-[Poppins]'>Making learning easier and <br /><span>more convenient for you.</span></div>
+        </div>
+    </div>
+  )
+}
