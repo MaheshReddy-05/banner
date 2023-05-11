63aea9e15fdcea8680f4d56e1814c483ab4851a3
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-05-11 16:23:28 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Banner from "./components/banner/Banner";
 import Navbar from "./components/nav/Navbar";
 import SocialMedia from "./components/Cards/SocialMedia"
-import Scp from './components/testimons/Scp';
+import Scp from "./components/testimons/Scp";
 
 function App() {
   return (
@@ -25,7 +25,9 @@ function App() {
         </div>
       </div>
       <div className="bg-[#111827]">
+        <div className="mx-40">
           <Scp></Scp>
+        </div>
       </div>
 
       
--- src/components/testimons/Scp.jsx
+++ src/components/testimons/Scp.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function Scp() {
   return (
     <div>
-        <div className='text-center text-white'>
-            <div className='text-[1rem] pt-10 font-[Rubik]'>WHY CODING-DECODED ?</div>
-            <br />
-            <div className='text-[2rem] font-[Poppins]'>Making learning easier and <br /><span>more convenient for you.</span></div>
+      <div className='text-center text-white'>
+        <div className='pt-10'>
+          {TextBetweenLines()}
         </div>
+        <br />
+        <div className='text-[2rem] font-[Poppins] mx-auto'>
+          Making learning easier and <br />
+          <span>more convenient for you.</span>
+        </div>
+      </div>
     </div>
-  )
+  );
+}
+
+function TextBetweenLines() {
+  return (
+    <div className='text-between-lines flex items-center'>
+      <div className='line flex-grow border-b border-gray-400 w-1/4'></div>
+      <div className='text-[1.5rem] mx-4 text-white font-[Rubik] px-20'>{"Why Coding-Decoded ?"}</div>
+      <div className='line flex-grow border-b border-gray-400 w-1/4'></div>
+    </div>
+  );
 }
