316779ef9095102438461bc4fd7980d5b901d900
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-05-11 11:53:39 +0530

    Social Media Dashboard

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -4,11 +4,15 @@ function Card() {
   const [youtubeFollowers, setYoutubeFollowers] = useState(0);
   const [instagramFollowers, setInstagramFollowers] = useState(0);
   const [linkedinFollowers, setLinkedinFollowers] = useState(0);
+  const [faangHires, setFaangHires] = useState(0);
+  const [telegramFollowers, setTelegramFollowers] = useState(0);
 
   // Set your desired follower counts
   const youtubeCount = 1000;
   const instagramCount = 500;
   const linkedinCount = 200;
+  const faangCount = 300;
+  const telegramCount = 400;
 
   useEffect(() => {
     const animateFollowers = () => {
@@ -33,6 +37,20 @@ function Card() {
           }
           return prevFollowers;
         });
+
+        setFaangHires((prevFollowers) => {
+          if (prevFollowers < faangCount) {
+            return prevFollowers + 1;
+          }
+          return prevFollowers;
+        });
+
+        setTelegramFollowers((prevFollowers) => {
+          if (prevFollowers < telegramCount) {
+            return prevFollowers + 1;
+          }
+          return prevFollowers;
+        });
       }, 10); // Adjust the interval as per your desired animation speed
 
       return () => clearInterval(interval);
@@ -43,25 +61,33 @@ function Card() {
 
   return (
     <div className="bg-gray-800 py-4">
-              <h1 className="text-white text-lg flex items-center">Social Media Followers</h1>
-      <div className="container mx-auto px-4  flex items-center ">
-        <div className="flex items-center justify-between">
+      <div className="container mx-auto px-4">
+        <div className=" justify-between">
+        <div className="flex items-center justify-center">
+          <h1 className="text-white text-center text-3xl whitespace-nowrap">Social Media Followers</h1>
+          <br></br>
+          <br></br>
+        </div>
           <div className="flex items-center">
-            <div className="mr-4">
+            <div className="basis-1/5 text-center mr-4">
+              <h2 className="text-white text-2xl font-bold">{youtubeFollowers}</h2>
+              <p className="text-gray-400 text-sm">YouTube Followers</p>
             </div>
-            <div className="flex items-center">
-              <div className="mr-4">
-                <h2 className="text-white text-2xl font-bold">{youtubeFollowers}</h2>
-                <p className="text-gray-400 text-sm">YouTube Followers</p>
-              </div>
-              <div className="mr-4">
-                <h2 className="text-white text-2xl font-bold">{instagramFollowers}</h2>
-                <p className="text-gray-400 text-sm">Instagram Followers</p>
-              </div>
-              <div>
-                <h2 className="text-white text-2xl font-bold">{linkedinFollowers}</h2>
-                <p className="text-gray-400 text-sm">LinkedIn Followers</p>
-              </div>
+            <div className="basis-1/5 text-center mr-4">
+              <h2 className="text-white text-2xl font-bold">{instagramFollowers}</h2>
+              <p className="text-gray-400 text-sm">Instagram Followers</p>
+            </div>
+            <div className="basis-1/5 text-center">
+              <h2 className="text-white text-2xl font-bold">{linkedinFollowers}</h2>
+              <p className="text-gray-400 text-sm">LinkedIn Followers</p>
+            </div>
+            <div className="basis-1/5 text-center">
+              <h2 className="text-white text-2xl font-bold">{faangHires}</h2>
+              <p className="text-gray-400 text-sm">People Got into FAANG</p>
+            </div>
+            <div className="basis-1/5 text-center">
+              <h2 className="text-white text-2xl font-bold">{telegramFollowers}</h2>
+              <p className="text-gray-400 text-sm">Telegram Followers</p>
             </div>
           </div>
         </div>
