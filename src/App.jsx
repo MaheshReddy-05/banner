28270d1f37c83f84ea85acf2d56d6ed423014c5f
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-05-17 15:38:49 +0530

    Grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Navbar from "./components/nav/Navbar";
 import SocialMedia from "./components/Cards/SocialMedia"
 import Scp from "./components/testimons/Scp";
 import Footer from './components/footer/Footer';
+import GridComponent from './components/Cards/GridComponent';
 
 function App() {
   return (
-    <div className='w-full overflow-hidden'>
+    <div className='w-full  overflow-hidden'>
       <div className="bg-[#111827]">
         <div className="">
           <Navbar></Navbar>
@@ -30,13 +31,16 @@ function App() {
           <Scp></Scp>
         </div>
       </div>
+      <div className='bg-[#111827]'>
+        <div className='mx-40 flex justify-center'>
+          <GridComponent></GridComponent>
+        </div>
+      </div>
       <div className="bg-[#1f2937]">
         <div className="mx-40">
           <Footer></Footer>
         </div>
       </div>
-
-      
     </div>
   );
 }
---
+++ src/components/Cards/GridComponent.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const GridComponent = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4 py-8">
+      {/* First Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+
+      {/* Second Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+
+      {/* Third Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+
+      {/* Fourth Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        {/* ... */}
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+
+      {/* Fifth Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        {/* ... */}
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+
+      {/* Sixth Grid Item */}
+      <div className="border bg-[#6881a5] border-gray-300 px-4 py-12 flex flex-col items-center justify-center shadow-md rounded-lg">
+        {/* ... */}
+        <div className="w-12 h-12 bg-gray-300 rounded-full mb-2"></div>
+        <div className="text-center">Data structures are the problem-solving pillars of coding. Learn all the foundational knowledge about all of the popular data structure that you need, to be well-versed in interviews.</div>
+      </div>
+    </div>
+  );
+};
+
+export default GridComponent;
