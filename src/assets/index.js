895e50e4c24e554298613a43affce3bb504e136d
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-05-31 22:44:48 +0530

    CLC

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,7 @@ import CB from "./images/JCF/CB.png"
 import FC from "./images/JCF/FC.png"
 import Gen from "./images/JCF/Gen.png"
 import GT from "./images/JCF/GT.png"
+import DS from "./images/WCD/DataStructures.jpeg"
 export {
   ban,
   linkedin,
@@ -17,4 +18,5 @@ export {
   FC,
   Gen,
   GT,
+  DS,
 };
--- src/components/whatyouget/WYG.jsx
+++ src/components/whatyouget/WYG.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { LLD } from "../lotti/Lotti.jsx";
+import { DS } from "../../assets";
 export default function () {
   return (
     <div>
       <div className="mx-auto max-w-6xl px-4 sm:px-6 md:my-24 lg:my-24 lg:px-8">
-        <div >
+        <div>
           <div className="text-center">
             <div className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-               Why Coding Decoded?
+              Why Coding Decoded?
             </div>
             <div className="mt-4 text-2sm leading-6 tracking-wide text-gray-500">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
@@ -23,18 +24,19 @@ export default function () {
                   Data Structures
                 </div>
                 <div className="text-[16px] pt-5">
-                A strong understanding of data structures allows candidates to solve complex problems and demonstrate 
-                their ability to write efficient code. We have detailed step by step detailed videos to 
-                build DSA concepts (Link to SDE revision sheet) 
+                  A strong understanding of data structures allows candidates to
+                  solve complex problems and demonstrate their ability to write
+                  efficient code. We have detailed step by step detailed videos
+                  to build DSA concepts (Link to SDE revision sheet)
                 </div>
               </div>
-              <div className="col-span-1 my-auto ">
-                <LLD></LLD>
+              <div className="col-span-1 m-auto w-80 ">
+                <img src={DS} alt="Your Image" class="rounded-2xl shadow-3xl" />
               </div>
             </div>
             <div className="grid grid-cols-2 ">
-              <div className="col-span-1 my-auto">
-                <LLD></LLD>
+            <div className="col-span-1 m-auto w-80 ">
+                <img src={DS} alt="Your Image" class="rounded-2xl shadow-3xl" />
               </div>
               <div className="col-span-1 my-auto">
                 <div className="text-3xl font-bold leading-tight text-rose-500 sm:text-4xl lg:text-[30px]">
@@ -64,13 +66,13 @@ export default function () {
                   and originality.
                 </div>
               </div>
-              <div className="col-span-1 my-auto">
-                <LLD></LLD>
+              <div className="col-span-1 m-auto w-80 ">
+                <img src={DS} alt="Your Image" class="rounded-2xl shadow-3xl" />
               </div>
             </div>
             <div className="grid grid-cols-2  ">
-              <div className="col-span-1 my-auto ">
-                <LLD></LLD>
+            <div className="col-span-1 m-auto w-80 ">
+                <img src={DS} alt="Your Image" class="rounded-2xl shadow-3xl" />
               </div>
               <div className="col-span-1 my-auto">
                 <div className="text-3xl font-bold leading-tight text-rose-500 sm:text-4xl lg:text-[30px]">
