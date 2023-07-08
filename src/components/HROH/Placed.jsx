8b13e580d071b824da541bc481098c1b39fec94b
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-07-09 02:36:25 +0530

    pics updated

--- src/components/HROH/Placed.jsx
+++ src/components/HROH/Placed.jsx
@@ -2,11 +2,86 @@ import { useState, useEffect, useRef } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 import Iteam from "../slider/Iteam";
+import DeepeshGodara from "../../assets/images/HROH/DeepeshGodara.jpg"
+import HimanshuYadav from "../../assets/images/HROH/HimanshuYadav.jpg"
+import AnilSharma from "../../assets/images/HROH/AnilSharma.jpg"
+import YeshwanthChintaginjala from "../../assets/images/HROH/YeshwanthChintaginjala.jpg"
+import NancyAgarwal from "../../assets/images/HROH/NancyAgarwal.jpg"
+import AdamKniffin from "../../assets/images/HROH/AdamKniffin.jpg"
+import YajasSardana from "../../assets/images/HROH/YajasSardana.jpg"
+import AbhishekJain from "../../assets/images/HROH/AbhishekJ.jpg"
+import SonalKumar from "../../assets/images/HROH/SonalKumar.jpg"
 
 export default function Placed() {
   const splideRef = useRef(null);
   const [activeSlide, setActiveSlide] = useState(0);
   const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const teamData = [
+    {
+      id: 1,
+      image: DeepeshGodara,
+      name: "Deepesh Godara",
+      role: "",
+      about: "",
+    },
+    {
+      id: 2,
+      image: HimanshuYadav,
+      name: "Himanshu Yadav",
+      role: "",
+      about: "",
+    },
+    {
+      id: 3,
+      image: AnilSharma,
+      name: "Anil Sharma",
+      role: "",
+      about: "",
+    },
+    {
+      id: 4,
+      image: YeshwanthChintaginjala,
+      name: "Yeshwanth Chintaginjala",
+      role: "",
+      about: "",
+    },
+    {
+      id: 5,
+      image: NancyAgarwal,
+      name: "Nancy Agarwal",
+      role: "",
+      about: "",
+    },
+    {
+      id: 6,
+      image: AdamKniffin,
+      name: "Adam Kniffin",
+      role: "",
+      about: "",
+    },
+    {
+      id: 7,
+      image: YajasSardana,
+      name: "Yajas Sardana",
+      role: "",
+      about: "",
+    },
+    {
+      id: 8,
+      image: AbhishekJain,
+      name: "Abhishek Jain",
+      role: "",
+      about: "",
+    },
+    {
+      id: 9,
+      image: SonalKumar,
+      name: "Sonal Kumar",
+      role: "",
+      about: "",
+    },
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -53,27 +128,11 @@ export default function Placed() {
         }}
         onMoved={onSlideMoved}
       >
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
-        <SplideSlide>
-          <Iteam></Iteam>
-        </SplideSlide>
+        {teamData.map((person) => (
+          <SplideSlide key={person.id}>
+            <Iteam Im={person.image} name={person.name} role={person.role} about={person.about} />
+          </SplideSlide>
+        ))}
       </Splide>
     </div>
   );
--- src/components/slider/Iteam.jsx
+++ src/components/slider/Iteam.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function TeamItem(){
+export function TeamItem(probs){
   return (
     <div>
       <a href="https://www.linkedin.com/company/codingdecoded/" target="_blank">
@@ -8,11 +8,11 @@ export function TeamItem(){
       <div className="col-sm-6 col-md-4">
         <div className="team-item bg-gray-100 text-center mx-auto my-8 px-4 py-6 rounded transition-colors duration-250 ease-in-out hover:bg-blue-300 hover:text-white">
           <img
-            src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg"
+            src={probs.Im}
             className="team-img w-32 h-32 mx-auto rounded-full p-1 bg-gray-400"
             alt="pic"
           />
-          <h3 className="text-lg font-semibold mt-4 mb-1">JOHNATHAN HAWKINS</h3>
+          <h3 className="text-lg font-semibold mt-4 mb-1">{probs.name}</h3>
           <div className="team-info">
             <p className="text-gray-600">Head of SEO</p>
           </div>
